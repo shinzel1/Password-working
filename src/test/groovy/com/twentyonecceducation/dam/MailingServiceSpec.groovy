aedbee612f52df2d996c8225d5ee9eb5c3c41b2a
sachin vinay <75784102+shinzel1@users.noreply.github.com>
2021-10-08 14:16:53 +0530

    Add files via upload

---
+++ src/test/groovy/com/twentyonecceducation/dam/MailingServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.twentyonecceducation.dam
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class MailingServiceSpec extends Specification implements ServiceUnitTest<MailingService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ src/test/groovy/com/twentyonecceducation/dam/RegistrationServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.twentyonecceducation.dam
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class RegistrationServiceSpec extends Specification implements ServiceUnitTest<RegistrationService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
